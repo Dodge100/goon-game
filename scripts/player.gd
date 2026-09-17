912b21a90e11c7721a3779dcd4c4999a69338178
ByteAtATime <byteatatime@proton.me>
2026-09-16 18:37:56 -0700

    feat: add basic character movement

---
+++ scripts/player.gd
@@ -0,0 +1,21 @@
+class_name IsometricPlayer
+extends CharacterBody2D
+
+@export var max_speed: float = 500.0
+@export var acceleration: float = 2500.0
+@export var friction: float = 3000.0
+
+func _physics_process(delta: float) -> void:
+	var input_direction: Vector2 = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+
+	if input_direction != Vector2.ZERO:
+		velocity = velocity.move_toward(input_direction * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+
+	move_and_slide()
