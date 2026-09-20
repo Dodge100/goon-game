5a8943893cab03e4107b9d506d631fc568e4e19a
fily-gif <zeroad69@gmail.com>
2026-09-20 15:04:02 -0400

    entrances and exits detection

--- scripts/dungeon.gd
+++ scripts/dungeon.gd
@@ -7,9 +7,16 @@ func _ready() -> void:
 		if maplayer is TileMapLayer:
 			maps.append(maplayer)
 	for map in maps.size():
-		print(map, ": ", maps[map].get_used_cells())
+		print(map, ": ", maps[map].get_used_cells().size(), find_entrace_exits(maps[map]))
 	return
-# test commit sdfsdf
+
+func find_entrace_exits(_map: TileMapLayer) -> Array[Vector2i]:
+	var out: Array[Vector2i] = []
+	for cell in _map.get_used_cells():
+		var td := _map.get_cell_tile_data(cell)
+		if td and td.get_custom_data("corridor"):
+			out.append(cell)
+	return out
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
