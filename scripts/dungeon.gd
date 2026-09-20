10a2ef567a189b759cc803f138021cf4f5077091
fily-gif <zeroad69@gmail.com>
2026-09-19 23:35:20 -0400

    started figuring out rooms

---
+++ scripts/dungeon.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# array of TileMapLayers
+var maps = []
+func _ready() -> void:
+	for maplayer in get_children():
+		if maplayer is TileMapLayer:
+			maps.append(maplayer)
+	for map in maps.size():
+		print(map, ": ", maps[map].get_used_cells())
+	return
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
